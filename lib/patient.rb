82c56734d0d7f5eda691a28d167410b2709cab90
Chris Wade <swade781977@yahoo.com>
2020-02-08 02:06:52 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -12,6 +12,9 @@ class Patient
     @@all
   end
   
+  def new_appointment(doctor, date)
+    Appointment.new(date, self, doctor)
+  
   def appointments
     Appointment.all.select{|app| app.patient == self}
   end
